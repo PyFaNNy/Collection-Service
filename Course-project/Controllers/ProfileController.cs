162923a0a43be67e01074e8692521a47e441254d
Artyom <fanny.koob@gmail.com>
2021-02-05 02:32:11 +0300

    add sort on profile page

--- Course-project/Controllers/ProfileController.cs
+++ Course-project/Controllers/ProfileController.cs
@@ -24,7 +24,7 @@ namespace Course_project.Controllers
         }
 
         // GET: ProfileController
-        public async Task<ActionResult> Index(string userId, string name)
+        public async Task<ActionResult> Index(string userId, string name, SortState sortOrder = SortState.NameAscending)
         {
             User user = new User();
             if (userId != null)
@@ -37,9 +37,32 @@ namespace Course_project.Controllers
                 user = await _userManager.FindByNameAsync(name);
                 ViewBag.User = user;
             }
-            
-            var collections = _context.Collections.Where(p => p.UserId.Equals(user.Id)).ToList();
-            return View(collections);
+            IQueryable<Collection> collections = _context.Collections.Where(p => p.UserId.Equals(user.Id));
+            ViewData["NameSort"] = sortOrder == SortState.NameAscending ? SortState.NameDescendingly : SortState.NameAscending;
+            ViewData["CountSort"] = sortOrder == SortState.CountAscending ? SortState.CountDescendingly : SortState.CountAscending;
+            ViewData["ThemeSort"] = sortOrder == SortState.ThemeAscending ? SortState.ThemeDescendingly : SortState.ThemeAscending;
+            switch (sortOrder)
+            {
+                case SortState.NameAscending:
+                    collections = collections.OrderBy(s => s.Name);
+                    break;
+                case SortState.NameDescendingly:
+                    collections = collections.OrderByDescending(s => s.Name);
+                    break;
+                case SortState.CountDescendingly:
+                    collections = collections.OrderByDescending(s => s.CountItems);
+                    break;
+                case SortState.ThemeAscending:
+                    collections = collections.OrderBy(s => s.Theme);
+                    break;
+                case SortState.ThemeDescendingly:
+                    collections = collections.OrderByDescending(s => s.Theme);
+                    break;
+                default:
+                    collections = collections.OrderBy(s => s.CountItems);
+                    break;
+            }
+            return View(collections.AsNoTracking().ToList());
         }
 
     }
